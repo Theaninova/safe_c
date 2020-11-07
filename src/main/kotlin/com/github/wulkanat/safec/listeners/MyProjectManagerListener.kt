6ee892e09b3de00dc83c76ffe0767fdfd40e5ef0
GitHub Action <action@github.com>
2020-11-07 22:46:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/wulkanat/safec/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/wulkanat/safec/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wulkanat.safec.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.wulkanat.safec.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/wulkanat/safec/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wulkanat/safec/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wulkanat.safec.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wulkanat.safec.MyBundle
 
 class MyApplicationService {
 
